b4c0b8b39f7f2e40f08509e5ed882e8ce3e6241b
dan.muresan <dan.muresan@orange.com>
2025-09-29 14:33:59 +0300

    Adaugare cookie

--- assets/js/script.js
+++ assets/js/script.js
@@ -424,17 +424,3 @@ document.addEventListener('DOMContentLoaded', () => {
         cookieFloatingBtn.style.display = 'flex';
     }
 });
-document.addEventListener("DOMContentLoaded", () => {
-  const btn = document.getElementById("servicesBtn");
-  const text = document.getElementById("servicesText");
-
-  btn.addEventListener("click", () => {
-    if (text.style.display === "none" || text.style.display === "") {
-      text.style.display = "block";
-      btn.textContent = "Read Less";
-    } else {
-      text.style.display = "none";
-      btn.textContent = "Read More";
-    }
-  });
-});
